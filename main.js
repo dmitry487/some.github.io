780afde812f2a2758c27069eb4f45a601f8feb6a
ПЛАТИНОВЫЙ МОРФЛИНГ АНТОНА ДУРАЧЬЁ <153272210+dmitry487@users.noreply.github.com>
2024-08-24 13:50:44 +0300

    no more

--- main.js
+++
@@ -1,8 +0,0 @@
-const menu = document.querySelector('.menu');
-const button = document.querySelector('.menu_menu');
-
-function toggleMenu() {
-  menu.classList.toggle('menu--active');
-}
-
-button.addEventListener('click', toggleMenu)
