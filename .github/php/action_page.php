650eae2f019be2aa4591f41b89de200ffa4a5292
ПЛАТИНОВЫЙ МОРФЛИНГ АНТОНА ДУРАЧЬЁ <153272210+dmitry487@users.noreply.github.com>
2024-09-26 10:51:35 +0300

    sdfsdf

--- .github/php/action_page.php
+++ .github/php/action_page.php
@@ -4,5 +4,9 @@ require_once('users.txt');
 $login - $_POST['Username'];
 $pass - $_POST['Password'];
 
-
+if (empty($login) || empty($pass)){
+    echo "warning";
+} else {
+    
+}
 $sql - "INSERT INTO 'users' (Username,Password) VALUES ('$login', '$pass')";
